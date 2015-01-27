d1f8eb45493db17ddf3a99332a237516c1c10c88
Soskin Victoria <victoria.s@b-e.org.il>
2015-01-27 11:00:41 +0200

    uncommit the plog

--- TTF/TTFSite.Master.cs
+++ TTF/TTFSite.Master.cs
@@ -12,21 +12,21 @@ namespace TTF
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //if (!IsPostBack)
-            //{
-            //    string pg = HttpContext.Current.Request.Url.AbsoluteUri;
-            //    string cs = System.Configuration.ConfigurationManager.ConnectionStrings["BEBook10"].ConnectionString;
-            //    SqlConnection cc = new SqlConnection(cs);
-            //    SqlCommand cD = new SqlCommand(string.Format("IF EXISTS(SELECT * FROM p0t_APPClosed WHERE LogActive = 1) \n" +
-            //        "INSERT PLog(Page,UserId,SessionId,Logtime)VALUES('{0}',{1},'{2}',GETDATE())",
-            //        pg.Replace("'", "''").Substring(0, (pg.Length > 500 ? 500 : pg.Length)),
-            //        (HttpContext.Current.Session["Userid"] == null ? 0 : (int)HttpContext.Current.Session["UserID"]),
-            //        HttpContext.Current.Session.SessionID.Replace("'", "''"))
-            //        , cc);
-            //    cc.Open();
-            //    cD.ExecuteNonQuery();
-            //    cc.Close();
-            //}
+            if (!IsPostBack)
+            {
+                string pg = HttpContext.Current.Request.Url.AbsoluteUri;
+                string cs = System.Configuration.ConfigurationManager.ConnectionStrings["BEBook10"].ConnectionString;
+                SqlConnection cc = new SqlConnection(cs);
+                SqlCommand cD = new SqlCommand(string.Format("IF EXISTS(SELECT * FROM p0t_APPClosed WHERE LogActive = 1) \n" +
+                    "INSERT PLog(Page,UserId,SessionId,Logtime)VALUES('{0}',{1},'{2}',GETDATE())",
+                    pg.Replace("'", "''").Substring(0, (pg.Length > 500 ? 500 : pg.Length)),
+                    (HttpContext.Current.Session["Userid"] == null ? 0 : (int)HttpContext.Current.Session["UserID"]),
+                    HttpContext.Current.Session.SessionID.Replace("'", "''"))
+                    , cc);
+                cc.Open();
+                cD.ExecuteNonQuery();
+                cc.Close();
+            }
 
         }
     }
